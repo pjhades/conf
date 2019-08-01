a46a140e026936a7a2fad8be6209b06b26ff469b
Jing Peng <pj.hades@gmail.com>
2019-07-31 20:44:55 -0400

    Improve keyboard backlight script, add autorandr

--- scripts/keyboard-backlight.py
+++ scripts/keyboard-backlight.py
@@ -6,8 +6,9 @@
 
 import dbus
 import sys
+import re
 
-def setbacklight(delta_percent):
+def setbacklight(value, symbol):
     bus = dbus.SystemBus()
     proxy = bus.get_object('org.freedesktop.UPower',
                            '/org/freedesktop/UPower/KbdBacklight')
@@ -16,17 +17,34 @@ def setbacklight(delta_percent):
 
     current = interface.GetBrightness()
     maximum = interface.GetMaxBrightness()
-    delta = delta_percent / 100 * maximum
-    new = max(0, min(current + delta, maximum))
+
+    if not symbol:
+        new = value / 100 * maximum
+    else:
+        multiplier = 1 if symbol == '+' else -1
+        delta = value / 100 * maximum * multiplier
+        new = max(0, min(current + delta, maximum))
+
     assert(0 <= new <= maximum)
 
     interface.SetBrightness(new)
     return (current, maximum)
 
 if __name__ ==  '__main__':
-    if len(sys.argv) != 2 or (sys.argv[1] != 'up' and sys.argv[1] != 'down'):
-        print('usage: {} <up|down>'.format(sys.argv[0]))
+    if len(sys.argv) != 2:
+        print('usage: {} PERCENT[+|-]'.format(sys.argv[0]))
         exit(1)
 
-    current, maximum = setbacklight(10 if sys.argv[1] == 'up' else -10)
+    match = re.match(r'^(\d+)(\+|-)?$', sys.argv[1])
+    if not match:
+        print('value should be in format PERCENT[+|-]')
+        exit(1)
+
+    value, symbol = match.groups()
+    value = int(value)
+    if not 0 <= value <= 100:
+        print('value should be between 0 and 100 inclusively')
+        exit(1)
+
+    current, maximum = setbacklight(value, symbol)
     print('current {} ({}), maximum {}'.format(current, current / maximum, maximum))
